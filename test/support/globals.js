4a7df8b6a78bf3af6b94507689736490363e67da
Blake Embrey <hello@blakeembrey.com>
2014-11-24 21:59:17 -0600

    Add polyfill on node

--- test/support/globals.js
+++ test/support/globals.js
@@ -1,4 +1,8 @@
 if (typeof exports === 'object') {
+  if (!global.Promise) {
+    require('es6-promise').polyfill();
+  }
+
   global.popsicle = require('../..');
   global.expect   = require('chai').expect;
   global.FormData = require('form-data');
