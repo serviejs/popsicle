637e63bf6f5131d371055141be4b657b540a0934
Gareth Hunt <gareth-hunt@rocketmail.com>
2016-03-29 22:48:55 -0700

    Export `CookieJar` for `tough-cookie` browser override
    Closes #47

--- lib/browser/tough-cookie.ts
+++ lib/browser/tough-cookie.ts
@@ -1,3 +1,5 @@
-export = function ToughCookie () {
-  throw new TypeError('Cookie jars are only available on node')
+export class CookieJar {
+  constructor () {
+    throw new TypeError('Cookie jars are only available on node')
+  }
 }
