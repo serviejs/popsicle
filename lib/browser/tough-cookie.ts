8c690781af65ec4a65f0df8bde1e1478e074ddd8
Gareth Hunt <gareth-hunt@rocketmail.com>
2016-03-29 22:48:55 -0700

    Export CookieJar for tough-cookie browser typings override. Avoids tsc error

--- lib/browser/tough-cookie.ts
+++ lib/browser/tough-cookie.ts
@@ -1,3 +1,11 @@
-export = function ToughCookie () {
+function ToughCookie () {
   throw new TypeError('Cookie jars are only available on node')
 }
+
+export class CookieJar {
+  constructor() {
+    ToughCookie();
+  }
+}
+
+export default ToughCookie;
